472bc1d8afe078757aa9a0751465595511caa752
神奇的Sean <admin@mail.imsean.cn>
2021-08-13 10:20:19 +0800

    Update Rewrite.js

--- Scripts/Rewrite.js
+++ Scripts/Rewrite.js
@@ -1,26 +1,40 @@
-hostname = api.weibo.cn, mapi.weibo.com, *.uve.weibo.com, mp.weixin.qq.com, api.bilibili.com, app.bilibili.com, www.zhihu.com, api.zhihu.com, link.zhihu.com
+hostname = api.weibo.cn, mapi.weibo.com, *.uve.weibo.com, mp.weixin.qq.com, api.bilibili.com, app.bilibili.com, www.zhihu.com, api.zhihu.com, link.zhihu.com, api.m.jd.com, homepage-api.smzdm.com, haojia-api.smzdm.com, article-api.smzdm.com
+
 
 # 去微博应用内广告 (By yichahucha)
-^https?://(sdk|wb)app.uve.weibo.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Weibo/weibo_launch.js
-^https?://m?api.weibo.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)(mix)?timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|searchall|cardlist|page|!/photos/pic_recommend_status|video/tiny_stream_video_list) url script-response-body https://raw.githubusercontent.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Weibo/weibo_ad.js
+^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/wb_launch.js
+^https?://m?api\.weibo\.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)(mix)?timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|searchall|cardlist|page|!/photos/pic_recommend_status|video/tiny_stream_video_list|photo/info) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js
 
-# 去微信公众号广告 (By Choler)
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Wechat/Wechat.js
+# 去微信公众号广告 (By Choler\NobyDa)
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+^(http|https):\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url response-body "advertisement_num":\d,"advertisement_info":\[.+\], response-body "advertisement_num":0,"advertisement_info":[],
 
 # 知乎去广告 (By onewayticket255)
 https://api.zhihu.com/(ad|drama|fringe|commercial|market/popover|search/(top|preset|tab)|.*featured-comment-ad) url reject-200
-https://api.zhihu.com/people/ url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Zhihu/page.js
-https://api.zhihu.com/moments/recommend url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Zhihu/Feed.js
-https://api.zhihu.com/topstory/recommend url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Zhihu/Recommend.js
-https://api.zhihu.com/v4/questions url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Zhihu/Answer.js
+https://api.zhihu.com/people/ url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20people.js
+https://api.zhihu.com/moments/recommend url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20feed.js
+https://api.zhihu.com/topstory/recommend url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20recommend.js
+https://api.zhihu.com/v4/questions url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20answer.js
+https?://link.zhihu.com/\?target= url script-request-header https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20link.js
+
+# 什么值得买
+^https:\/\/homepage-api.smzdm.com\/home url script-response-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_remove_ads.js
+^https:\/\/haojia-api.smzdm.com\/home\/list url script-response-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_remove_ads.js
+^https:\/\/article-api.smzdm.com\/article\/index_home_page url script-response-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/smzdm/smzdm_remove_ads.js
 
 # 哔哩哔哩动画去广告 (By onewayticket255)
 https://app.bilibili.com/x/v2/(splash|search/(defaultword|square)) url reject-200
 https://api.bilibili.com/x/v2/dm/ad url reject-200
-#https://app.bilibili.com/x/v2/space\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibilisurge%20bilibili%20space.js
-https://app.bilibili.com/x/resource/show/tab\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibili/Tab.js
-https://app.bilibili.com/x/v2/feed/index\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibili/Feed.js
-https://app.bilibili.com/x/v2/account/mine\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibili/Account.js
-https://app.bilibili.com/x/v2/view\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibili/View_Relate.js
-https://api.bilibili.com/x/v2/reply/main\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibili/Reply.js
-https://api.live.bilibili.com/xlive/app-room/v1/index/getInfoByRoom\?access_key url script-response-body https://raw.sevencdn.com/sve1r/Rules-For-Quantumult-X/develop/Scripts/AdBlock/Bilibili/Live.js
+https://app.bilibili.com/x/v2/space\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20space.js
+https://app.bilibili.com/x/resource/show/tab\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20tab.js
+https://app.bilibili.com/x/v2/feed/index\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20feed.js
+https://app.bilibili.com/x/v2/account/mine\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20account.js
+https://app.bilibili.com/x/v2/view\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20view%20relate.js
+https://api.bilibili.com/x/v2/reply/main\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%20reply.js
+https://api.live.bilibili.com/xlive/app-room/v1/index/getInfoByRoom\?access_key url script-response-body https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20bilibili%
+
+# 哔哩哔哩番剧开启1080P+
+^https:\/\/ap(p|i)\.bilibili\.com\/((pgc\/player\/api\/playurl)|(x\/v2\/account\/myinfo\?)|(x\/v2\/account/mine\?)) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/bilifj.js
+
+# 京东比价
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/jd_price.js
